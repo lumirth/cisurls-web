b47ad226da3161b19d79e438271cecf888b319d3
lumirth <65358837+lumirth@users.noreply.github.com>
2023-03-29 21:22:03 -0500

    complete overhaul, move main functionality into module, add courses.illinois.edu feature

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-This project fixes the broken URLs used in the examples in the [UIUC CIS API documentation](https://courses.illinois.edu/cisdocs/api). This project also holds the `cisapi.xsd` schema file([mirth.cc/fix-cisapi/cisapi.xsd](https://mirth.cc/fix-cisapi/cisapi.xsd)), which is a broken link in the documentation and cannot be found elsewhere as of 2023-03-22. 
+This project is an interface for the [cisurls module](https://www.npmjs.com/package/cisurls). It can fix URLs used in the examples in the [UIUC Course Information Suite API documentation](https://courses.illinois.edu/cisdocs/api). This project also holds the `cisapi.xsd` schema file([mirth.cc/cisurls/cisapi.xsd](https://mirth.cc/fix-cisapi/cisapi.xsd)), which is a broken link in the documentation and cannot be found elsewhere as of 2023-03-22. 
 
-The tool fixes two issues:
+This tool can do two things:
+- Fix the broken example URLs in the documentation
+- Convert working `cisapp/explorer` URLs to `courses.illinois.edu` URLs
+
+The documentation fix tool fixes two issues:
 - Change “/cisapi” to “/cisapp/explorer”
 - Add `.xml` before the parameters. For example, change “/courses” to “/courses.xml”
 
--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,66 @@
 <script>
-  function modifyUrl(url, addCascade = false) {
-    if (!url) {
+  // use uiuc-cis-urls package
+  import { fixDocumentationURL, convertCourseURL } from "cisurls";
+
+  let errMsgDoc;
+  let errMsgConv;
+  function modfifyDocumentationURL(url, addCascade = false) {
+    // try and catch error then show error in UI
+    try {
+      errMsgDoc = "";
+      return fixDocumentationURL(url, addCascade);
+    } catch (error) {
+      errMsgDoc = error.message;
       return "";
     }
-
-    // Special case: if url matches https://courses.illinois.edu/cisapi/schedule/courses?year=2012&term=spring§ionTypeCode=LEC§ionTypeCode=Q&collegeCode=KV&creditHours=3&subject=CHEM&sessionId=1&gened=NAT&qp=atomic+structure
-    // Then add ampersands before each section symbol and make §ionTypeCode=LEC§ionTypeCode=Q into &sectionTypeCode=LEC&sectionTypeCode=Q
-    const specialUrlPattern =
-      /^https:\/\/courses\.illinois\.edu\/cisapi\/schedule\/courses\?.*$/;
-    if (specialUrlPattern.test(url)) {
-      url = url.replace(/§/g, "&sect");
-    }
-
-    // Replace "/cisapi" with "/cisapp/explorer"
-    url = url.replace("/cisapi", "/cisapp/explorer");
-
-    // Add ".xml" before the parameters if it doesn't already exist
-    if (!url.includes(".xml")) {
-      const [baseUrl, queryParams] = url.split("?");
-      url = queryParams ? `${baseUrl}.xml?${queryParams}` : `${baseUrl}.xml`;
-    }
-    // Add "mode=cascade" parameter if requested
-    // Replace "mode=*" with "mode=cascade" if it already exists
-    // Add "mode=cascade" with & or ? depending on if there are already parameters
-    if (addCascade) {
-      if (url.includes("mode=")) {
-        // Replace "mode=*" with "mode=cascade" if it already exists.
-        url = url.replace(/mode=[^&]*/, "mode=cascade");
-      } else {
-        // Add "mode=cascade" with & or ? depending on if there are already parameters.
-        url += url.includes("?") ? "&mode=cascade" : "?mode=cascade";
-      }
-    }
-
-    return url;
   }
 
-  let originalUrl = "";
-  let modifiedUrl = "";
-  let copied = false;
+  let originalUrlDoc = "";
+  let modifiedUrlDoc = "";
+  let copiedDoc = false;
+  let originalUrlConv = "";
+  let convertedUrlConv = "";
+  let copiedConv = false;
 
-  function handleSubmit(event) {
+  function handleSubmitDoc(event) {
     event.preventDefault();
-    modifiedUrl = modifyUrl(originalUrl);
-    copied = false; // Reset copied flag when URL is modified
+    modifiedUrlDoc = modfifyDocumentationURL(originalUrlDoc);
+    copiedDoc = false; // Reset copied flag when URL is modified
   }
 
-  function handleCopy() {
-    navigator.clipboard.writeText(modifiedUrl);
-    copied = true; // Set copied flag to display message
+  function handleCopyDoc() {
+    navigator.clipboard.writeText(modifiedUrlDoc);
+    copiedDoc = true; // Set copied flag to display message
   }
 
-  function handleModifyWithCascade() {
-    modifiedUrl = modifyUrl(originalUrl, true);
-    copied = false; // Reset copied flag when URL is modified
+  function handleModifyWithCascadeDoc() {
+    modifiedUrlDoc = modfifyDocumentationURL(originalUrlDoc, true);
+    copiedDoc = false; // Reset copied flag when URL is modified
   }
 
-  function handleVisit() {
-    window.open(modifiedUrl, "_blank");
+  function handleVisitDoc() {
+    window.open(modifiedUrlDoc, "_blank");
+  }
+
+  async function handleConvertSubmit(event) {
+    event.preventDefault();
+    try {
+      errMsgConv = "";
+      convertedUrlConv = await convertCourseURL(originalUrlConv);
+    } catch (error) {
+      errMsgConv = error.message;
+      convertedUrlConv = "";
+    }
+    copiedConv = false; // Reset copied flag when URL is converted
+  }
+
+  function handleCopyConv() {
+    navigator.clipboard.writeText(convertedUrlConv);
+    copiedConv = true; // Set copied flag to display message
+  }
+
+  function handleVisitConv() {
+    window.open(convertedUrlConv, "_blank");
   }
 </script>
 
@@ -65,9 +68,7 @@
   <ul>
     <li>
       More information can be found in the Github repo for this app at
-      <a href="https://github.com/lumirth/fix-cisapi/"
-        >github.com/lumirth/fix-cisapi</a
-      >.
+      <a href="https://github.com/lumirth/cisurls-web/">lumirth/cisurls-web</a>.
     </li>
     <li>
       The UIUC CIS API documentation can be found on
@@ -80,46 +81,174 @@
       like when this app was created at
       <a
         href="https://web.archive.org/web/20160517202637/courses.illinois.edu/cisdocs/api"
-        >web.archive.org/web/20160517202637/courses.illinois.edu/cisdocs/api</a
+        >web.archive.org</a
       >.
     </li>
     <li>
       The XML schema for the UIUC CIS API can be found at
-      <a href="https://mirth.cc/fix-cisapi/cisapi.xsd"
-        >mirth.cc/fix-cisapi/cisapi.xsd.</a
-      > This schema cannot be found elsewhere, as the link in the documentation is
-      broken.
+      <a href="https://mirth.cc/cisurls/cisapi.xsd">/cisapi.xsd.</a> This schema
+      cannot be found elsewhere, as the link in the documentation is broken.
     </li>
   </ul>
-  <br />
-  <form on:submit={handleSubmit}>
-    <label for="originalUrl">Enter URL:</label>
-    <input type="text" id="originalUrl" bind:value={originalUrl} />
+  <hr />
+  <!-- First form for convertCourseURL -->
+  <form on:submit={handleSubmitDoc}>
+    <label for="originalUrl"
+      >This tool converts documentation <code>cisapi</code> URLs to functional
+      <code>cisapp/explorer</code> URLs:</label
+    >
+    <br />
+    <input
+      type="text"
+      id="originalUrl"
+      placeholder="ENTER DOCUMENTATION URL"
+      bind:value={originalUrlDoc}
+    />
 
     <button type="submit">MODIFY URL</button>
-    <button type="button" on:click={handleModifyWithCascade}>
+    <button type="button" on:click={handleModifyWithCascadeDoc}>
       MODIFY W/ CASCADE
     </button>
   </form>
+  {#if errMsgDoc}
+    <p>{errMsgDoc}</p>
+  {/if}
 
-  {#if modifiedUrl}
-    <p>The modified URL is: {modifiedUrl}</p>
-    <button on:click={handleCopy}>
-      {#if copied}
+  {#if modifiedUrlDoc}
+    <p>The modified URL is: {modifiedUrlDoc}</p>
+    <button on:click={handleCopyDoc}>
+      {#if copiedDoc}
         COPIED
       {:else}
         COPY
       {/if}
     </button>
-    <button type="button" on:click={handleVisit}>VISIT</button>
+    <button type="button" on:click={handleVisitDoc}>VISIT</button>
   {/if}
+  <hr />
+  <!-- Second form for convertCourseURL -->
+  <form on:submit={handleConvertSubmit}>
+    <label for="originalUrl"
+      >This tool converts <code>cisapp/explorer/schedule</code> course URLs to
+      <code>courses.illinois.edu</code> URLs:</label
+    >
+    <br />
+    <input
+      type="text"
+      id="originalUrlConv"
+      placeholder="ENTER COURSE URL"
+      bind:value={originalUrlConv}
+    />
+
+    <button type="submit">CONVERT COURSE URL</button>
+  </form>
+  {#if errMsgConv}
+    <p>{errMsgConv}</p>
+  {/if}
+
+  {#if convertedUrlConv}
+    <p>The converted URL is: {convertedUrlConv}</p>
+    <button on:click={handleCopyConv}>
+      {#if copiedConv}
+        COPIED
+      {:else}
+        COPY
+      {/if}
+    </button>
+    <button type="button" on:click={handleVisitConv}>VISIT</button>
+  {/if}
+  <hr />
+  <p style="text-align:center;">
+    A <a href="https://mirth.cc">lumirth</a> tool.
+  </p>
 </main>
 
 <style>
+  /* google inter font */
+  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap");
+  /* google fira code font */
+  @import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
+  main {
+    margin: 1rem;
+    font-family: "Inter", sans-serif;
+    /* max-width and centering */
+    max-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 4rem;
+  }
+  code {
+    background-color: #f8f8f8;
+    border: 1px solid #e7e7e7;
+    border-radius: 0.5rem;
+    padding: 0.1rem 0.3rem;
+  }
+  button {
+    border-radius: 0.2rem;
+    cursor: pointer;
+    margin-left: 0.1rem;
+    margin-right: 0.1rem;
+    border: 1px solid #f8f8f8;
+    /* border-radius: 0.2rem; */
+    padding: 0.1rem 0.3rem;
+    background-color: #e7e7e7;
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    transition: all 0.1s ease-in-out;
+  }
+  button:hover {
+    border: 1px solid #000;
+    transition: all 0.1s ease-in-out;
+  }
+  input {
+    background-color: #e7e7e7;
+    border-radius: 0.2rem;
+    width: 225px;
+    margin-top: 1rem;
+    outline: none;
+    border: 1px solid transparent;
+    padding: 0.1rem 0.3rem;
+  }
+  input::placeholder {
+    color: #000;
+    opacity: 0.3;
+  }
+  input:focus::placeholder {
+    color: transparent;
+    transition: all 1s ease-in-out;
+  }
+  input:focus {
+    border: 1px solid #000;
+    transition: all 0.1s ease-in-out;
+  }
+  a {
+    color: #000;
+    text-decoration: underline;
+    text-underline-offset: 0.15rem;
+    text-decoration-thickness: 0.075rem;
+    transition: all 0.2s ease-in-out;
+  }
+  a:hover {
+    text-decoration: underline;
+    text-underline-offset: 0.35rem;
+    text-decoration-thickness: 0.075rem;
+    color: #ff0000;
+    transition: all 0.2s ease-in-out;
+  }
+  hr {
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    border: 0;
+    border-top-color: currentcolor;
+    border-top-style: none;
+    border-top-width: 0px;
+    border-top: 1px solid #e7e7e7;
+    margin: 2rem 0;
+  }
   ul {
     padding-left: 1rem;
   }
   ul li {
-    margin-bottom: 0.25rem;
+    margin-bottom: 0.5rem;
   }
 </style>
