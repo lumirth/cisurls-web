a673c815dcb054d94c680aa6140bded9d48fc648
lumirth <65358837+lumirth@users.noreply.github.com>
2023-03-31 21:48:02 -0500

    make her sexy

--- src/App.svelte
+++ src/App.svelte
@@ -63,63 +63,85 @@
   function handleVisitConv() {
     window.open(convertedUrlConv, "_blank");
   }
+
+  function changeSummaryText() {
+    const summary = document.querySelector("summary");
+    const code = summary.querySelector("code");
+    if (!code) return;
+    if (code.textContent === "DETAILS ABOUT THIS TOOL ▼") {
+      code.textContent = "DETAILS ABOUT THIS TOOL ▲";
+    } else {
+      code.textContent = "DETAILS ABOUT THIS TOOL ▼";
+    }
+  }
 </script>
 
 <main class="page__content">
-  <ul>
-    <li>
-      More information can be found in the Github repo for this app at
-      <a
-        class="link link--gasping"
-        href="https://github.com/lumirth/cisurls-web/">lumirth/cisurls-web</a
-      >.
-    </li>
-    <li>
-      The UIUC CIS API documentation can be found on
-      <a
-        class="link link--gasping"
-        href="https://courses.illinois.edu/cisdocs/api"
-        >courses.illinois.edu/cisdocs/api.</a
-      >
-    </li>
-    <li>
-      You can find a link to the an archive of what the documentation looked
-      like when this app was created at
-      <a
-        class="link link--gasping"
-        href="https://web.archive.org/web/20160517202637/courses.illinois.edu/cisdocs/api"
-        >web.archive.org</a
-      >.
-    </li>
-    <li>
-      The XML schema for the UIUC CIS API can be found at
-      <a
-        class="link link--gasping"
-        href="https://mirth.cc/cisurls-web/cisapi.xsd">/cisapi.xsd.</a
-      > This schema cannot be found elsewhere, as the link in the documentation is
-      broken.
-    </li>
-  </ul>
+  <details>
+    <summary on:click={changeSummaryText} style="cursor:pointer;"
+      ><code class="text--big-technical code--inline"
+        >DETAILS ABOUT THIS TOOL ▼</code
+      ></summary
+    >
+    <ul>
+      <li>
+        More information can be found in the Github repo for this app at
+        <a
+          class="link link--gasping"
+          href="https://github.com/lumirth/cisurls-web/">lumirth/cisurls-web</a
+        >.
+      </li>
+      <li>
+        The UIUC CIS API documentation can be found on
+        <a
+          class="link link--gasping"
+          href="https://courses.illinois.edu/cisdocs/api"
+          >courses.illinois.edu/cisdocs/api.</a
+        >
+      </li>
+      <li>
+        You can find a link to the an archive of what the documentation looked
+        like when this app was created at
+        <a
+          class="link link--gasping"
+          href="https://web.archive.org/web/20160517202637/courses.illinois.edu/cisdocs/api"
+          >web.archive.org</a
+        >.
+      </li>
+      <li>
+        The XML schema for the UIUC CIS API can be found at
+        <a
+          class="link link--gasping"
+          href="https://mirth.cc/cisurls-web/cisapi.xsd">/cisapi.xsd.</a
+        > This schema cannot be found elsewhere, as the link in the documentation
+        is broken.
+      </li>
+    </ul>
+  </details>
   <hr class="hori-line--with-margin" />
   <!-- First form for convertCourseURL -->
   <form on:submit={handleSubmitDoc}>
     <label for="originalUrl"
-      >This tool converts documentation <code class="code--inline">cisapi</code> URLs to functional
+      >This tool converts documentation <code class="code--inline">cisapi</code>
+      URLs to functional
       <code class="code--inline">cisapp/explorer</code> URLs:</label
     >
     <br />
     <div class="input-and-buttons-container">
-
       <input
-      class="input-text-line"
-      type="text"
-      id="originalUrl"
-      placeholder="ENTER DOCUMENTATION URL"
-      bind:value={originalUrlDoc}
+        class="input-text-line"
+        type="text"
+        id="originalUrl"
+        placeholder="ENTER DOCUMENTATION URL"
+        bind:value={originalUrlDoc}
       />
-      
+
       <button class="button" type="submit">MODIFY URL</button>
-      <button class="button" type="button" on:click={handleModifyWithCascadeDoc}>
+      <button
+        class="button"
+        type="button"
+        on:click={handleModifyWithCascadeDoc}
+      >
         MODIFY W/ CASCADE
       </button>
     </div>
@@ -129,35 +151,42 @@
   {/if}
 
   {#if modifiedUrlDoc}
-    <p>The modified URL is: {modifiedUrlDoc}</p>
-    <button class="button"  on:click={handleCopyDoc}>
-      {#if copiedDoc}
-        COPIED
-      {:else}
-        COPY
-      {/if}
-    </button>
-    <button class="button" type="button" on:click={handleVisitDoc}>VISIT</button>
+    <p>The modified URL is:</p>
+    <pre class="code--block">{modifiedUrlDoc}</pre>
+    <div class="input-and-buttons-container">
+      <button class="button" on:click={handleCopyDoc}>
+        {#if copiedDoc}
+          COPIED
+        {:else}
+          COPY
+        {/if}
+      </button>
+      <button class="button" type="button" on:click={handleVisitDoc}
+        >VISIT</button
+      >
+    </div>
   {/if}
   <hr class="hori-line--with-margin" />
   <!-- Second form for convertCourseURL -->
   <form on:submit={handleConvertSubmit}>
     <label for="originalUrl"
-      >This tool converts <code class="code--inline">cisapp/explorer/schedule</code> course URLs to
+      >This tool converts <code class="code--inline"
+        >cisapp/explorer/schedule</code
+      >
+      course URLs to
       <code class="code--inline">search</code> URLs:</label
     >
     <br />
     <div class="input-and-buttons-container">
+      <input
+        class="input-text-line"
+        type="text"
+        id="originalUrlConv"
+        placeholder="ENTER COURSE URL"
+        bind:value={originalUrlConv}
+      />
 
-    <input
-      class="input-text-line"
-      type="text"
-      id="originalUrlConv"
-      placeholder="ENTER COURSE URL"
-      bind:value={originalUrlConv}
-    />
-
-    <button class="button"  type="submit">CONVERT COURSE URL</button>
+      <button class="button" type="submit">CONVERT COURSE URL</button>
     </div>
   </form>
   {#if errMsgConv}
@@ -165,15 +194,20 @@
   {/if}
 
   {#if convertedUrlConv}
-    <p>The converted URL is: {convertedUrlConv}</p>
-    <button class="button"  on:click={handleCopyConv}>
-      {#if copiedConv}
-        COPIED
-      {:else}
-        COPY
-      {/if}
-    </button>
-    <button class="button"  type="button" on:click={handleVisitConv}>VISIT</button>
+    <p>The converted URL is:</p>
+    <pre class="code--block">{convertedUrlConv}</pre>
+    <div class="input-and-buttons-container">
+      <button class="button" on:click={handleCopyConv}>
+        {#if copiedConv}
+          COPIED
+        {:else}
+          COPY
+        {/if}
+      </button>
+      <button class="button" type="button" on:click={handleVisitConv}
+        >VISIT</button
+      >
+    </div>
   {/if}
   <hr class="hori-line--with-margin" />
   <p style="text-align:center;">
@@ -189,11 +223,26 @@
   li {
     margin-bottom: 0.5rem;
   }
-  
+
   label {
     line-height: 1;
   }
   .input-and-buttons-container {
     margin-top: 1rem;
+  }
+
+  .text--break-char {
+    word-break: break-all;
+  }
+
+  details {
+    margin-bottom: 1rem;
+  }
+
+  details > summary {
+    list-style: none;
+  }
+  details > summary::-webkit-details-marker {
+    display: none;
   }
 </style>
